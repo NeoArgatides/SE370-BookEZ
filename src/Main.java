9a5b6051714a644aaf313ca01b2f5ae7f36a169f
Neo Argatides <neo.argatides@gmail.com>
2022-11-29 11:30:47 -0800

    deleted database

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,8 @@
 import java.awt.EventQueue;
-//import java.sql.*;
-
 import javax.swing.JFrame;
 
 public class Main {
-    public static void main(String[] args) {
-    	/*try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", "user1", "pass");
-    	         Statement stmt = conn.createStatement();
-    	      ) {		      
-    	         String sql = "CREATE DATABASE";
-    	         stmt.executeUpdate(sql);
-    	         System.out.println("Database created successfully...");   	  
-    	      } catch (SQLException e) {
-    	         e.printStackTrace();
-    	      }
-    	
-    	/*Connection c = null;
-	    ///SE370-BookEZ/src
-	    try {
-	       Class.forName("org.sqlite.JDBC");
-	       c = DriverManager.getConnection("jdbc:sqlite:account.db");
-	    } catch ( Exception e ) {
-	       System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	       System.exit(0);
-	    }
-	    System.out.println("Opened database successfully");*/
-    	
+    public static void main(String[] args) {    	
     	EventQueue.invokeLater(new Runnable()
 		{
 			public void run()
