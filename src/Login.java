0791f00d4548e54672ec124d9fcf006864020c82
Neo Argatides <neo.argatides@gmail.com>
2022-12-02 16:20:46 -0800

    login class

---
+++ src/Login.java
@@ -0,0 +1,181 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class Login extends JPanel {
+	private final JButton registerBtn = new JButton("Register");
+	private final JButton loginBtn = new JButton("Login");
+	private final JLabel titleLbl = new JLabel("BookEZ");
+	private final JLabel loginLbl = new JLabel("Please enter username and password");
+	private final JPanel textFieldPanel = new JPanel();
+	private final JTextField usernameTextField = new JTextField();
+	private final JTextField passwordTextField = new JTextField();
+	private final JPanel buttonPanel = new JPanel();
+	private final JPanel titlePanel = new JPanel();
+	private final JLabel errorLbl = new JLabel("");
+	private final JLabel usernameLbl = new JLabel("Username:");
+	private final JLabel passwordLbl = new JLabel("Password:");
+	private MainFrame mainFrame;
+	
+	Login(MainFrame mainFrame) {
+		this.mainFrame = mainFrame;
+		setLayout(new BorderLayout(0, 0));
+		setBackground(new Color(153, 204, 255));
+		textFieldPanel.setBackground(new Color(153, 204, 255));
+		add(textFieldPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_textFieldPanel = new GridBagLayout();
+		gbl_textFieldPanel.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_textFieldPanel.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_textFieldPanel.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_textFieldPanel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		textFieldPanel.setLayout(gbl_textFieldPanel);
+		
+		GridBagConstraints gbc_usernameLbl = new GridBagConstraints();
+		gbc_usernameLbl.insets = new Insets(0, 0, 5, 5);
+		gbc_usernameLbl.anchor = GridBagConstraints.EAST;
+		gbc_usernameLbl.gridx = 2;
+		gbc_usernameLbl.gridy = 1;
+		textFieldPanel.add(usernameLbl, gbc_usernameLbl);
+		usernameTextField.setColumns(10);
+		usernameTextField.setPreferredSize(new Dimension(400, 24));
+		GridBagConstraints gbc_usernameTextField = new GridBagConstraints();
+		gbc_usernameTextField.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_usernameTextField.insets = new Insets(0, 0, 5, 0);
+		gbc_usernameTextField.gridx = 3;
+		gbc_usernameTextField.gridy = 1;
+		textFieldPanel.add(usernameTextField, gbc_usernameTextField);
+		usernameTextField.setUI(new HintTextFieldUI(" Username", true));
+		
+		GridBagConstraints gbc_passwordLbl = new GridBagConstraints();
+		gbc_passwordLbl.insets = new Insets(0, 0, 5, 5);
+		gbc_passwordLbl.anchor = GridBagConstraints.EAST;
+		gbc_passwordLbl.gridx = 2;
+		gbc_passwordLbl.gridy = 2;
+		textFieldPanel.add(passwordLbl, gbc_passwordLbl);
+		passwordTextField.setColumns(10);
+		passwordTextField.setPreferredSize(new Dimension(400, 24));
+		GridBagConstraints gbc_passwordTextField = new GridBagConstraints();
+		gbc_passwordTextField.insets = new Insets(0, 0, 5, 0);
+		gbc_passwordTextField.anchor = GridBagConstraints.NORTHWEST;
+		gbc_passwordTextField.gridx = 3;
+		gbc_passwordTextField.gridy = 2;
+		textFieldPanel.add(passwordTextField, gbc_passwordTextField);
+		passwordTextField.setUI(new HintTextFieldUI(" Password", true));
+		
+		GridBagConstraints gbc_errorLbl = new GridBagConstraints();
+		gbc_errorLbl.gridx = 3;
+		gbc_errorLbl.gridy = 3;
+		errorLbl.setHorizontalAlignment(SwingConstants.RIGHT);
+		textFieldPanel.add(errorLbl, gbc_errorLbl);
+		add(buttonPanel, BorderLayout.SOUTH);
+		buttonPanel.setLayout(new GridLayout(1, 0, 0, 0));
+		buttonPanel.add(registerBtn);
+		buttonPanel.add(loginBtn);
+		titlePanel.setBackground(new Color(204, 204, 255));
+		add(titlePanel, BorderLayout.NORTH);
+		titlePanel.setLayout(new GridLayout(0, 1, 0, 0));
+		titlePanel.add(titleLbl);
+		titleLbl.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		titleLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		titlePanel.add(loginLbl);
+		loginLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		errorLbl.setForeground(new Color(178, 34, 34));
+		registerBtn.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		String username = usernameTextField.getText();
+        		String password = passwordTextField.getText();
+        		if(!username.equals("") && !password.equals("") && !usernameTaken(username)) {
+        			BufferedWriter bf = null;
+        			try {
+						bf = new BufferedWriter(new FileWriter("accounts.txt", true));
+						bf.write(username + ',' + password + ",\n");
+						bf.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+        			mainFrame.login(username);
+        		} else {
+        			errorLbl.setText("Invalid username and/or password");
+        			//System.out.println("taken");
+        		}
+        	}
+        });
+		loginBtn.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		String username = usernameTextField.getText();
+        		String password = passwordTextField.getText();
+        		Scanner sc = null;
+        		try {
+        			sc = new Scanner(mainFrame.getFile());
+        		} catch (FileNotFoundException e1) {
+        			e1.printStackTrace();
+        		}
+        		
+        		String line;
+        		int indexUsername;
+        		while(sc.hasNextLine()) {
+        			line = sc.nextLine();
+        			indexUsername = line.indexOf(",");
+        			if(line.substring(0, indexUsername).equals(username) && line.substring(indexUsername+1, line.indexOf(",", indexUsername+1)).equals(password)) {
+        				mainFrame.login(username);
+        				break;
+        			}
+        		}
+        		if(!sc.hasNextLine()) {
+        			errorLbl.setText("Invalid username and/or password");
+        		}
+        		
+        	}
+        });
+		setVisible(true);
+		
+		
+	}
+	
+	void login() {
+		usernameTextField.setText("");
+		passwordTextField.setText("");
+		errorLbl.setText("");
+	}
+	
+	boolean usernameTaken(String username) {
+		Scanner sc = null;
+		try {
+			sc = new Scanner(mainFrame.getFile());
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+		String line;
+		while(sc.hasNextLine()) {
+			line = sc.nextLine();
+			if(line.substring(0 , line.indexOf(",")).equals(username)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -31,18 +31,11 @@ public class MainFrame extends JFrame {
 	File file = new File("accounts.txt");
 	String currentUser;
 	private JPanel mainPanel = new JPanel();
-	private JPanel loginPanel = new JPanel();
+	private JPanel loginPanel = new Login(this);
 	private JPanel menuPanel = new JPanel();
+	private JPanel uploadPanel = new JPanel();
 	private CardLayout cl = new CardLayout();
-	private final JButton registerBtn = new JButton("Register");
-	private final JButton loginBtn = new JButton("Login");
-	private final JLabel titleLbl = new JLabel("BookEZ");
-	private final JLabel loginLbl = new JLabel("Please enter username and password");
-	private final JPanel textFieldPanel = new JPanel();
-	private final JTextField usernameTextField = new JTextField();
-	private final JTextField passwordTextField = new JTextField();
-	private final JPanel buttonPanel = new JPanel();
-	private final JPanel titlePanel = new JPanel();
+	
 	private final JPanel menuTitlePanel = new JPanel();
 	private final JLabel menuTitleLbl = new JLabel("BookEZ");
 	private final JLabel menuInstructLbl = new JLabel("Menu");
@@ -53,104 +46,24 @@ public class MainFrame extends JFrame {
 	private final JButton menuTableBtn = new JButton("View ROI Table");
 	private final JPanel menuBottomPanel = new JPanel();
 	
+	private final JPanel uploadTitlePanel = new JPanel();
+	private final JLabel menuTitleLbl_1 = new JLabel("BookEZ");
+	private final JLabel uploadInstructLbl = new JLabel("Upload Data");
+	private final JPanel uploadButtonPanel = new JPanel();
+	private final JButton uploadBackBtn = new JButton("Back to Menu");
+	
+	
 	MainFrame() {
 		getContentPane().add(mainPanel);
 		mainPanel.setBackground(new Color(153, 204, 255));
 		mainPanel.setLayout(cl);
 		mainPanel.add(loginPanel, "1");
-		menuPanel.setBackground(new Color(153, 204, 255));
 		mainPanel.add(menuPanel, "2");
+		mainPanel.add(uploadPanel, "3");
+		menuPanel.setBackground(new Color(153, 204, 255));
 		menuPanel.setLayout(new BorderLayout(0, 0));
 		mainPanel.setVisible(true);
 		
-		//login page
-		loginPanel.setLayout(new BorderLayout(0, 0));
-		loginPanel.setBackground(new Color(153, 204, 255));
-		textFieldPanel.setBackground(new Color(153, 204, 255));
-		loginPanel.add(textFieldPanel, BorderLayout.CENTER);
-		GridBagLayout gbl_textFieldPanel = new GridBagLayout();
-		gbl_textFieldPanel.columnWidths = new int[]{450, 0};
-		gbl_textFieldPanel.rowHeights = new int[]{103, 103, 0};
-		gbl_textFieldPanel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_textFieldPanel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		textFieldPanel.setLayout(gbl_textFieldPanel);
-		usernameTextField.setColumns(10);
-		usernameTextField.setPreferredSize(new Dimension(400, 24));
-		GridBagConstraints gbc_usernameTextField = new GridBagConstraints();
-		gbc_usernameTextField.anchor = GridBagConstraints.SOUTHEAST;
-		gbc_usernameTextField.insets = new Insets(0, 0, 5, 0);
-		gbc_usernameTextField.gridx = 0;
-		gbc_usernameTextField.gridy = 0;
-		textFieldPanel.add(usernameTextField, gbc_usernameTextField);
-		passwordTextField.setColumns(10);
-		passwordTextField.setPreferredSize(new Dimension(400, 24));
-		GridBagConstraints gbc_passwordTextField = new GridBagConstraints();
-		gbc_passwordTextField.anchor = GridBagConstraints.NORTHEAST;
-		gbc_passwordTextField.gridx = 0;
-		gbc_passwordTextField.gridy = 1;
-		textFieldPanel.add(passwordTextField, gbc_passwordTextField);
-		usernameTextField.setUI(new HintTextFieldUI(" Username", true));
-		passwordTextField.setUI(new HintTextFieldUI(" Password", true));
-		loginPanel.add(buttonPanel, BorderLayout.SOUTH);
-		buttonPanel.setLayout(new GridLayout(1, 0, 0, 0));
-		buttonPanel.add(registerBtn);
-		buttonPanel.add(loginBtn);
-		titlePanel.setBackground(new Color(204, 204, 255));
-		loginPanel.add(titlePanel, BorderLayout.NORTH);
-		titlePanel.setLayout(new GridLayout(0, 1, 0, 0));
-		titlePanel.add(titleLbl);
-		titleLbl.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		titleLbl.setHorizontalAlignment(SwingConstants.CENTER);
-		titlePanel.add(loginLbl);
-		loginLbl.setHorizontalAlignment(SwingConstants.CENTER);
-		registerBtn.addActionListener(new ActionListener()
-        {
-        	public void actionPerformed(ActionEvent e)
-        	{
-        		String username = usernameTextField.getText();
-        		String password = passwordTextField.getText();
-        		if(!username.equals("") && !password.equals("") && !usernameTaken(username)) {
-        			BufferedWriter bf = null;
-        			try {
-						bf = new BufferedWriter(new FileWriter("accounts.txt", true));
-						bf.write(username + ',' + password + ",\n");
-						bf.close();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-        			login(username);
-        		} else {
-        			//System.out.println("taken");
-        		}
-        	}
-        });
-		loginBtn.addActionListener(new ActionListener()
-        {
-        	public void actionPerformed(ActionEvent e)
-        	{
-        		String username = usernameTextField.getText();
-        		String password = passwordTextField.getText();
-        		Scanner sc = null;
-        		try {
-        			sc = new Scanner(file);
-        		} catch (FileNotFoundException e1) {
-        			e1.printStackTrace();
-        		}
-        		
-        		String line;
-        		int indexUsername;
-        		while(sc.hasNextLine()) {
-        			line = sc.nextLine();
-        			indexUsername = line.indexOf(",");
-        			if(line.substring(0, indexUsername).equals(username) && line.substring(indexUsername+1, line.indexOf(",", indexUsername+1)).equals(password)) {
-        				login(username);
-        			}
-        		}
-        		
-        	}
-        });
-		loginPanel.setVisible(true);
 		cl.show(mainPanel, "1");
 		
 		//main menu
@@ -167,6 +80,13 @@ public class MainFrame extends JFrame {
 		menuPanel.add(menuBackPanel, BorderLayout.WEST);
 		
 		menuBackPanel.add(menuBackBtn);
+		menuBackBtn.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		cl.show(mainPanel, "1");
+        	}
+        });
 		menuOptionPanel.setBackground(new Color(153, 204, 255));
 		
 		menuPanel.add(menuOptionPanel, BorderLayout.CENTER);
@@ -175,36 +95,51 @@ public class MainFrame extends JFrame {
 		menuOptionPanel.add(menuTableBtn);
 		menuBottomPanel.setBackground(new Color(153, 204, 255));
 		
-		menuPanel.add(menuBottomPanel, BorderLayout.SOUTH);
+		/*menuPanel.add(menuBottomPanel, BorderLayout.SOUTH);
 		Icon imgIcon = new ImageIcon(this.getClass().getResource("enchanting.gif"));
 		JLabel menuBottomLbl = new JLabel(imgIcon);
 		menuBottomPanel.add(menuBottomLbl); 
+		*/
+		
+		//upload data
+		uploadPanel.setBackground(new Color(153, 204, 255));
+		uploadPanel.setLayout(new BorderLayout(0, 0));
+		uploadTitlePanel.setBackground(new Color(204, 204, 255));
+		
+		uploadPanel.add(uploadTitlePanel, BorderLayout.NORTH);
+		uploadTitlePanel.setLayout(new GridLayout(0, 1, 0, 0));
+		menuTitleLbl_1.setHorizontalAlignment(SwingConstants.CENTER);
+		menuTitleLbl_1.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		
+		uploadTitlePanel.add(menuTitleLbl_1);
+		uploadInstructLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		uploadTitlePanel.add(uploadInstructLbl);
+		uploadButtonPanel.setBackground(new Color(153, 204, 255));
+		
+		uploadPanel.add(uploadButtonPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_uploadButtonPanel = new GridBagLayout();
+		gbl_uploadButtonPanel.columnWidths = new int[]{0, 0};
+		gbl_uploadButtonPanel.rowHeights = new int[]{0, 0};
+		gbl_uploadButtonPanel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_uploadButtonPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		uploadButtonPanel.setLayout(gbl_uploadButtonPanel);
+		
+		GridBagConstraints gbc_uploadBackBtn = new GridBagConstraints();
+		gbc_uploadBackBtn.gridx = 0;
+		gbc_uploadBackBtn.gridy = 0;
+		uploadButtonPanel.add(uploadBackBtn, gbc_uploadBackBtn);
 		
 		
 	}
 	
-	boolean usernameTaken(String username) {
-		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}
-		
-		String line;
-		while(sc.hasNextLine()) {
-			line = sc.nextLine();
-			if(line.substring(0 , line.indexOf(",")).equals(username)) {
-				return true;
-			}
-		}
-		return false;
+	File getFile() {
+		return file;
 	}
 	
 	void login(String username) {
 		cl.show(mainPanel, "2");
 		currentUser = username;
-		usernameTextField.setText("");
-		passwordTextField.setText("");
+		((Login) loginPanel).login();
 	}
 }
