7c83e1d48a685c20ac87f8db00579b753bf2d0b4
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2023-04-15 12:02:10 -0700

    my stuff

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 public class Main {
-    public static void main(String[] args) {    	
+    public static void main(String[] args) {
+		Create_DB database = Create_DB.getInstance();    	
     	EventQueue.invokeLater(new Runnable()
 		{
 			public void run()
