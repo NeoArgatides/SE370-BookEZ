f589a32e8e4150bd62d9f809e903c582499b0752
gabyr01 <gabriela00rojas@gmail.com>
2023-05-04 19:01:42 -0700

    hashed password and sorting fixed
    Co-authored-by: Scizmz <Scizmz@users.noreply.github.com>
    Co-authored-by: nagalil-honaj-hteal3 <nagalil-honaj-hteal3@users.noreply.github.com>
    Co-authored-by: NeoArgatides <NeoArgatides@users.noreply.github.com>

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -1,3 +1,6 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,16 +19,32 @@ public class UserDAO {
         ResultSet rs = null;
         User user = null;
         try {
-            stmt = conn.prepareStatement("SELECT * FROM users WHERE username=? AND password=?");
+            // stmt = conn.prepareStatement("SELECT * FROM users WHERE username=? AND password=?");
+            // stmt.setString(1, username);
+            // stmt.setString(2, password);
+            // rs = stmt.executeQuery();
+            // if (rs.next()) {
+            //     int id = rs.getInt("id");
+            //     String name = rs.getString("username");
+            //     String pass = rs.getString("password");
+            //     user = new User(id, name, pass);
+            // }
+            stmt = conn.prepareStatement("SELECT * FROM users WHERE username=?");
             stmt.setString(1, username);
-            stmt.setString(2, password);
             rs = stmt.executeQuery();
             if (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("username");
-                String pass = rs.getString("password");
-                user = new User(id, name, pass);
+            int id = rs.getInt("id");
+            String name = rs.getString("username");
+            String hashedPassword = rs.getString("password");
+            try {
+                if (verifyPassword(password, hashedPassword)) {
+                    user = new User(id, name, hashedPassword);
+                }
+            } catch (NoSuchAlgorithmException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
+        }
         } finally {
             if (rs != null) {
                 rs.close();
@@ -37,6 +56,21 @@ public class UserDAO {
         return user;
     }
 
+    //for password 
+    private String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hashedBytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashedBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+    
+    private boolean verifyPassword(String password, String hashedPassword) throws NoSuchAlgorithmException {
+        return hashedPassword.equals(hashPassword(password));
+    }
+    
     public boolean createUser(String username, String password) throws SQLException {
         PreparedStatement stmt = null;
         boolean success = false;
@@ -56,7 +90,7 @@ public class UserDAO {
         }
         
         // Insert new user
-        stmt = conn.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
+        stmt = conn.prepareStatement("INSERT INTO users (username, password) VALUES (?, SHA2(?, 256))");
         stmt.setString(1, username);
         stmt.setString(2, password);
         stmt.executeUpdate();
